28304ba963132548975f98ce4edb553b88ea069e
Ibrahim Mohammed <Ibrahimmohammed017@gmail.com>
2023-11-29 13:55:21 +0200

    last last last edit

--- js/main.js
+++ js/main.js
@@ -111,3 +111,26 @@ function chooseProtext() {
     $(pr).css("display", "flex");
   });
 }
+
+// No Copy Text & Images
+
+document.oncontextmenu = function () {
+  return false;
+};
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+};
